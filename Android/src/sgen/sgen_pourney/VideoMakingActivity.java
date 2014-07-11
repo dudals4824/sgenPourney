0258bd0867e081c94502c3d63b795e7af10b5017
ustia <ustia@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-11 09:40:56 +0000

    영상 만들기 화면 나오는거

---
+++ Android/src/sgen/sgen_pourney/VideoMakingActivity.java
@@ -0,0 +1,16 @@
+package sgen.sgen_pourney;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class VideoMakingActivity extends Activity {
+	LinearLayout layoutAlbum;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_video);
+		
+	}
+
+}
