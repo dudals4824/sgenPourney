a86ca05214e2030e2a5efc2a1e68c6604ebfb080
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-11 09:43:14 +0000

    PhotoputActivity

--- Android/src/sgen/sgen_pourney/PhotoputActivity.java
+++ Android/src/sgen/sgen_pourney/PhotoputActivity.java
@@ -1,47 +1,98 @@
 package sgen.sgen_pourney;
 
+import java.io.File;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.widget.LinearLayout;
-import android.os.Build;
 
 public class PhotoputActivity extends Activity {
+	static final int SELECT_PICTURE = 1;
 	LinearLayout layoutAlbum;
+	private Uri currImageURI;
+	private String imagePath;
+	private File imgFile;
+	private Bitmap mBitmap;
+	private int tevelTerm;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_photoput);
-		layoutAlbum=(LinearLayout)findViewById(R.id.layoutAlbum);
-		
-		layoutAlbum.addView(new DayAlbumCell(this));
-		
+		layoutAlbum = (LinearLayout) findViewById(R.id.layoutAlbum);
+
+		layoutAlbum.addView(new DayAlbum(PhotoputActivity.this));
+		tevelTerm=3;
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		switch (resultCode) {
+		case SELECT_PICTURE:
+			currImageURI = data.getData();
+			Log.d("KJK", "URI : " + currImageURI.toString());
+			// 실제 절대주소를 받아옴
+			imagePath = getRealPathFromURI(currImageURI);
+			Log.d("KJK", "URI : " + currImageURI.toString());
+			Log.d("KJK", "Real Path : " + imagePath);
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.photoput, menu);
-		return true;
-	}
+			// image path 얻어왔으면 imgFile초기화.
+			imgFile = new File(imagePath);
+			// img file bitmap 변경
+			if (imgFile.exists()) {
+				mBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+				// getCroppedBitmap(mBitmap);
+				Log.e("비트맵 로드", "성공");
+			} else
+				Log.e("비트맵 디코딩", "실패");
+			
+			
+			BitmapDrawable bd = (BitmapDrawable) this.getResources()
+					.getDrawable(R.drawable.i_photo_gray_mask318x318);
+			Bitmap coverBitmap = bd.getBitmap();
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+			// constructor
+			// mBitmap에 찍은 사진 넣기
+			// cover은 그대로
+
+//			photoEditor photoEdit = new photoEditor(mBitmap, coverBitmap,
+//					photoAreaWidth, photoAreaHeight);
+//			// resize
+//			// crop roun
+//			// overay cover
+//
+//			// 이거하면 이미지 셋됨
+//			mBitmap = photoEdit.editPhotoAuto();
+//			btn_picbtn.setImageBitmap(mBitmap);
+			break;
+
+		default:
+			break;
 		}
-		return super.onOptionsItemSelected(item);
+	}
+
+	private String getRealPathFromURI(Uri contentUri) {
+		String path = null;
+		String[] proj = { MediaStore.MediaColumns.DATA };
+		Cursor cursor = getContentResolver().query(contentUri, proj, null,
+				null, null);
+		if (cursor.moveToFirst()) {
+			int column_index = cursor
+					.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+			path = cursor.getString(column_index);
+		}
+		cursor.close();
+		return path;
 	}
 
 }
