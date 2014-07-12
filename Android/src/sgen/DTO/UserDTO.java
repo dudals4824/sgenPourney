fab2a511b4887efe7e7a44f4f3da94e0fdea23a4
kj0604 <kj0604@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-12 09:35:40 +0000

    user dto 커밋

--- Android/src/sgen/DTO/UserDTO.java
+++ Android/src/sgen/DTO/UserDTO.java
@@ -1,5 +1,60 @@
 package sgen.DTO;
 
 public class UserDTO {
+	private int userId;
+	private String nickName;
+	private String email;
+	private String profileFilename;
+	
+	public UserDTO() {
+		super();
+	}
 
+	public UserDTO(int userId, String nickName, String email,
+			String profileFilename) {
+		super();
+		this.userId = userId;
+		this.nickName = nickName;
+		this.email = email;
+		this.profileFilename = profileFilename;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProfileFilename() {
+		return profileFilename;
+	}
+
+	public void setProfileFilename(String profileFilename) {
+		this.profileFilename = profileFilename;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", nickName=" + nickName
+				+ ", email=" + email + ", profileFilename=" + profileFilename
+				+ "]";
+	}
 }
