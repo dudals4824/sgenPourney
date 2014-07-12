dc5e8c92e1e25323848467ceec1b58d351a7312b
ustia <ustia@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-12 03:14:00 +0000

    비디오 생성에 현재시간 출력까지

--- Android/src/sgen/sgen_pourney/VideoMakingActivity.java
+++ Android/src/sgen/sgen_pourney/VideoMakingActivity.java
@@ -1,12 +1,25 @@
 package sgen.sgen_pourney;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class VideoMakingActivity extends Activity {
 	LinearLayout layoutAlbum;
+	private EditText timer;
+	// private String time;
+	final Calendar c = Calendar.getInstance();
+
+	int Hour = c.get(Calendar.HOUR_OF_DAY); // HOUR는 12시간, HOUR_OF_DAY는 24시간
+											// 입니다.
+	int Minute = c.get(Calendar.MINUTE);
+	int Second = c.get(Calendar.SECOND);
+	int AmPm = c.get(Calendar.AM_PM);
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,8 +27,9 @@ public class VideoMakingActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_video);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
-				R.layout.custom_title);		
-		
+				R.layout.custom_title);
+		timer = (EditText) findViewById(R.id.timer);
+		timer.setText(Hour + " : " + Minute + " : " + Second);
 		
 	}
 
