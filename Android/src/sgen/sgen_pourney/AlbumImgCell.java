a831d7b1be462f65806c56737abdd1137720b3a6
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-27 12:15:35 +0000

    커밋!!

---
+++ Android/src/sgen/sgen_pourney/AlbumImgCell.java
@@ -0,0 +1,47 @@
+package sgen.sgen_pourney;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class AlbumImgCell extends RelativeLayout{
+
+	private Context mContext = null;
+	private Bitmap mBitmap=null;
+	private ImageView imgPhoto=null;
+	public AlbumImgCell(Context context, Bitmap bitmap) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		initMarbleView(context,bitmap);
+	}
+	private void initMarbleView(Context context, Bitmap bitmap) {
+		mContext = context;
+		mBitmap=bitmap;
+		Log.d("AlbumImgCell", mBitmap.toString());
+		
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li = (LayoutInflater) getContext().getSystemService(
+				infService);
+		View v = li.inflate(R.layout.album_img_cell, this, false);
+		addView(v);
+		
+		//이미지를 비트맵으로 받아와서 이미지뷰에 추가 리사이징 해야함
+		imgPhoto=(ImageView)findViewById(R.id.imgPhoto);
+		imgPhoto.setImageBitmap(mBitmap);
+		
+		
+	}
+
+}
