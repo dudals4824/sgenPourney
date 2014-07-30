9b6e233d990e23a0b118a5286cc1e4d290fb8d82
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-07-30 02:59:01 +0000

    PhotoputActivity

--- Android/src/sgen/android/photoput/PhotoputActivity.java
+++ Android/src/sgen/android/photoput/PhotoputActivity.java
@@ -59,7 +59,7 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 	private GridLayout layoutGridPhotoAlbum;
 	private ArrayList<String> imageUrls;
     private DisplayImageOptions options;
-    private ImageAdapter imageAdapter;
+//   private ImageAdapter imageAdapter;
     protected ImageLoader imageLoader = ImageLoader.getInstance();
 
 	@Override
@@ -91,32 +91,32 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 		layoutGridPhotoAlbum = (GridLayout) findViewById(R.id.layoutGridPhotoAlbum);
 		tevelTerm = 3;
 		
-		//여기서부터 갤러리 이미지 다중 선택을 위해서
-		final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
-        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
-        Cursor imagecursor = managedQuery(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
-                null, orderBy + " DESC");
- 
-        this.imageUrls = new ArrayList<String>();
- 
-        for (int i = 0; i < imagecursor.getCount(); i++) {
-            imagecursor.moveToPosition(i);
-            int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);
-            imageUrls.add(imagecursor.getString(dataColumnIndex));
- 
-            System.out.println("=====> Array path => "+imageUrls.get(i));
-        }
- 
-        options = new DisplayImageOptions.Builder()
-            .showStubImage(R.drawable.i_memo_back)
-            .showImageForEmptyUri(R.drawable.i_memo)
-            .cacheInMemory()
-            .cacheOnDisc()
-            .build();
- 
-        imageAdapter = new ImageAdapter(this, imageUrls);
-		//여기까지
+//		//여기서부터 갤러리 이미지 다중 선택을 위해서
+//		final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+//        final String orderBy = MediaStore.Images.Media.DATE_TAKEN;
+//        Cursor imagecursor = managedQuery(
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+//                null, orderBy + " DESC");
+// 
+//        this.imageUrls = new ArrayList<String>();
+// 
+//        for (int i = 0; i < imagecursor.getCount(); i++) {
+//            imagecursor.moveToPosition(i);
+//            int dataColumnIndex = imagecursor.getColumnIndex(MediaStore.Images.Media.DATA);
+//            imageUrls.add(imagecursor.getString(dataColumnIndex));
+// 
+//            System.out.println("=====> Array path => "+imageUrls.get(i));
+//        }
+// 
+//        options = new DisplayImageOptions.Builder()
+//            .showStubImage(R.drawable.i_memo_back)
+//            .showImageForEmptyUri(R.drawable.i_memo)
+//            .cacheInMemory()
+//            .cacheOnDisc()
+//            .build();
+// 
+//        imageAdapter = new ImageAdapter(this, imageUrls);
+//		//여기까지
 	}
 
 	public void onClick(View v) {
@@ -225,90 +225,90 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 //        super.onStop();
 //    }
  
-    public void btnChoosePhotosClick(View v){
- 
-        ArrayList<String> selectedItems = imageAdapter.getCheckedItems();
-
-    }
- 
-    /*private void startImageGalleryActivity(int position) {
-        Intent intent = new Intent(this, ImagePagerActivity.class);
-        intent.putExtra(Extra.IMAGES, imageUrls);
-        intent.putExtra(Extra.IMAGE_POSITION, position);
-        startActivity(intent);
-    }*/
- 
-    public class ImageAdapter extends BaseAdapter {
- 
-        ArrayList<String> mList;
-        LayoutInflater mInflater;
-        Context mContext;
-        SparseBooleanArray mSparseBooleanArray;
- 
-        public ImageAdapter(Context context, ArrayList<String> imageList) {
-            // TODO Auto-generated constructor stub
-            mContext = context;
-            mInflater = LayoutInflater.from(mContext);
-            mSparseBooleanArray = new SparseBooleanArray();
-            mList = new ArrayList<String>();
-            this.mList = imageList;
- 
-        }
- 
-        public ArrayList<String> getCheckedItems() {
-            ArrayList<String> mTempArry = new ArrayList<String>();
- 
-            for(int i=0;i<mList.size();i++) {
-                if(mSparseBooleanArray.get(i)) {
-                    mTempArry.add(mList.get(i));
-                }
-            }
- 
-            return mTempArry;
-        }
- 
-        @Override
-        public int getCount() {
-            return imageUrls.size();
-        }
- 
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
- 
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
- 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
- 
-            if(convertView == null) {
-                convertView = mInflater.inflate(R.layout.row_multiphoto_item, null);
-            }
- 
-            CheckBox mCheckBox = (CheckBox) convertView.findViewById(R.id.checkBox1);
-            final ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView1);
- 
- 
- 
-            mCheckBox.setTag(position);
-            mCheckBox.setChecked(mSparseBooleanArray.get(position));
-            mCheckBox.setOnCheckedChangeListener(mCheckedChangeListener);
- 
-            return convertView;
-        }
- 
-        OnCheckedChangeListener mCheckedChangeListener = new OnCheckedChangeListener() {
- 
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // TODO Auto-generated method stub
-                mSparseBooleanArray.put((Integer) buttonView.getTag(), isChecked);
-            }
-        };
-    }
+//    public void btnChoosePhotosClick(View v){
+// 
+//        ArrayList<String> selectedItems = imageAdapter.getCheckedItems();
+//
+//    }
+// 
+//    /*private void startImageGalleryActivity(int position) {
+//        Intent intent = new Intent(this, ImagePagerActivity.class);
+//        intent.putExtra(Extra.IMAGES, imageUrls);
+//        intent.putExtra(Extra.IMAGE_POSITION, position);
+//        startActivity(intent);
+//    }*/
+// 
+//    public class ImageAdapter extends BaseAdapter {
+// 
+//        ArrayList<String> mList;
+//        LayoutInflater mInflater;
+//        Context mContext;
+//        SparseBooleanArray mSparseBooleanArray;
+// 
+//        public ImageAdapter(Context context, ArrayList<String> imageList) {
+//            // TODO Auto-generated constructor stub
+//            mContext = context;
+//            mInflater = LayoutInflater.from(mContext);
+//            mSparseBooleanArray = new SparseBooleanArray();
+//            mList = new ArrayList<String>();
+//            this.mList = imageList;
+// 
+//        }
+// 
+//        public ArrayList<String> getCheckedItems() {
+//            ArrayList<String> mTempArry = new ArrayList<String>();
+// 
+//            for(int i=0;i<mList.size();i++) {
+//                if(mSparseBooleanArray.get(i)) {
+//                    mTempArry.add(mList.get(i));
+//                }
+//            }
+// 
+//            return mTempArry;
+//        }
+// 
+//        @Override
+//        public int getCount() {
+//            return imageUrls.size();
+//        }
+// 
+//        @Override
+//        public Object getItem(int position) {
+//            return null;
+//        }
+// 
+//        @Override
+//        public long getItemId(int position) {
+//            return position;
+//        }
+// 
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+// 
+//            if(convertView == null) {
+//                convertView = mInflater.inflate(R.layout.row_multiphoto_item, null);
+//            }
+// 
+//            CheckBox mCheckBox = (CheckBox) convertView.findViewById(R.id.checkBox1);
+//            final ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView1);
+// 
+// 
+// 
+//            mCheckBox.setTag(position);
+//            mCheckBox.setChecked(mSparseBooleanArray.get(position));
+//            mCheckBox.setOnCheckedChangeListener(mCheckedChangeListener);
+// 
+//            return convertView;
+//        }
+// 
+//        OnCheckedChangeListener mCheckedChangeListener = new OnCheckedChangeListener() {
+// 
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                // TODO Auto-generated method stub
+//                mSparseBooleanArray.put((Integer) buttonView.getTag(), isChecked);
+//            }
+//        };
+//    }
 
 }
