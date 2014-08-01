f1d8fa576d8f9364ecc6d730033e8e080c06f8fb
kj0604 <kj0604@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-01 09:46:03 +0000

    유저정보 전역 객체 추가

--- Android/src/sgen/application/PourneyApplication.java
+++ Android/src/sgen/application/PourneyApplication.java
@@ -2,7 +2,11 @@ package sgen.application;
 
 import android.app.Application;
 import sgen.DTO.UserDTO;
-
+/** 
+ * @author Junki
+ * UserDTO를 전역으로 사용하기 위해 Application을 상속한 클래스.
+ * 선언하고 전역 객체 처럼 사용하면 된다.
+ */
 public class PourneyApplication extends Application {
 
 	private UserDTO loggedInUser;
