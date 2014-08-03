9417143b2c8d512e6858e4d85b8ccadbe8d1bd0a
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-03 08:38:06 +0000

    PhotoputActivity

--- Android/src/sgen/android/photoput/PhotoputActivity.java
+++ Android/src/sgen/android/photoput/PhotoputActivity.java
@@ -1,8 +1,11 @@
 package sgen.android.photoput;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+import sgen.android.multigallery.CustomGallery;
+import sgen.android.multigallery.PhotoInfo;
 import sgen.image.resizer.ImageResizer;
 import sgen.sgen_pourney.AskActivity;
 import sgen.sgen_pourney.CoverActivity;
@@ -11,6 +14,7 @@ import sgen.sgen_pourney.R;
 import sgen.sgen_pourney.SimpleSideDrawer;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -53,8 +57,9 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 	// 갤러리 사용을 위한 변수 선언
 	private ProgressDialog mLoagindDialog;
 	private GridView gridviewPhotoAlbum;
-//	private ImageAdapter mListAdapter;
-//	private ArrayList<ThumbImageInfo> mThumbImageInfoList;
+
+	// private ImageAdapter mListAdapter;
+	// private ArrayList<ThumbImageInfo> mThumbImageInfoList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -82,9 +87,9 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 		layoutAlbum = (LinearLayout) findViewById(R.id.layoutAlbum);
 		layoutAlbum.addView(new DayAlbum(PhotoputActivity.this));
 		gridviewPhotoAlbum = (GridView) findViewById(R.id.gridviewPhotoAlbum);
-		layoutGridPhotoAlbum=(GridLayout)findViewById(R.id.layoutGridPhotoAlbum);
+		layoutGridPhotoAlbum = (GridLayout) findViewById(R.id.layoutGridPhotoAlbum);
 
-		}
+	}
 
 	public void onClick(View v) {
 		if (v.getId() == R.id.ask_text) {
@@ -106,92 +111,107 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
-		Log.d("onActivityResult", "onActivityResult");
-		Log.d("onActivityResult", resultCode + "");
+		Log.d("PhotoputActivity", "onActivityResult");
+		Log.d("PhotoputActivity", resultCode + "");
 		if (resultCode == RESULT_OK) {
-			Log.d("onActivityResult", "requestCode==RESULT_OK");
-			if (requestCode == SELECT_PICTURE) {
-				currImageURI = data.getData();
-				Log.d("KJK", "URI : " + currImageURI.toString());
-				// �ㅼ젣 �덈�二쇱냼瑜�諛쏆븘��
-				imagePath = getRealPathFromURI(data.getData());
-				Log.d("KJK", "URI : " + currImageURI.toString());
-				Log.d("KJK", "Real Path : " + imagePath);
+			// Log.d("PhotoputActivity", "requestCode==RESULT_OK");
+			// if (requestCode == SELECT_PICTURE) {
+			// currImageURI = data.getData();
+			// Log.d("KJK", "URI : " + currImageURI.toString());
+			// // �ㅼ젣 �덈�二쇱냼瑜�諛쏆븘��
+			// imagePath = getRealPathFromURI(data.getData());
+			// Log.d("KJK", "URI : " + currImageURI.toString());
+			// Log.d("KJK", "Real Path : " + imagePath);
+			//
+			// // sample bitmaplist
+			// ArrayList<Bitmap> bitmapList = new ArrayList<Bitmap>();
+			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
+			// R.drawable.i_back));
+			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
+			// R.drawable.i_caledar));
+			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
+			// R.drawable.i_logo));
+			//
+			// // Log.d("path", imagePath);
+			// imgFile = new File(imagePath);
+			// // Log.d("path", storagePath);
+			// // 이 부분이 저장될 파일 위치
+			// storageFile = new File("/storage/emulated/0/DCIM/Camera",
+			// "a.png");
+			// Log.d("path", storageFile.toString());
+			// scaledBitmap = ImageResizer.resize(imgFile, 300, 300);
+			//
+			// Log.d("scaledBitmap", scaledBitmap + "");
+			// ImageResizer.saveToFile(scaledBitmap, storageFile);
+			//
+			// /* Log.d("resize","resizing!"); */
+			//
+			// // img file bitmap 蹂�꼍
+			// if (imgFile.exists()) {
+			// mBitmap = BitmapFactory.decodeFile(imgFile
+			// .getAbsolutePath());
+			// Log.d("mBitmap", imgFile.getAbsolutePath() + "");
+			// // getCroppedBitmap(mBitmap);
+			// for (int i = 0; i < bitmapList.size(); i++) {
+			// // 리사이징 안할거니까 imgFile없애야함
+			// layoutGridPhotoAlbum.addView(new AlbumImgCell(
+			// PhotoputActivity.this, bitmapList.get(i),
+			// imgFile));
+			// }
+			// Log.e("鍮꾪듃留�濡쒕뱶", "�깃났");
+			// }
+			//
+			// BitmapDrawable bd = (BitmapDrawable) this.getResources()
+			// .getDrawable(R.drawable.i_photo_gray_mask318x318);
+			// Bitmap coverBitmap = bd.getBitmap();
+			//
+			// // constructor
+			// // mBitmap��李띿� �ъ쭊 �ｊ린
+			// // cover��洹몃�濡�
+			//
+			// // photoEditor photoEdit = new photoEditor(mBitmap, coverBitmap,
+			// // photoAreaWidth, photoAreaHeight);
+			// // // resize
+			// // // crop roun
+			// // // overay cover
+			// //
+			// // // �닿굅�섎㈃ �대�吏��뗫맖
+			// // mBitmap = photoEdit.editPhotoAuto();
+			// // btn_picbtn.setImageBitmap(mBitmap);
 
-				// sample bitmaplist
-				ArrayList<Bitmap> bitmapList = new ArrayList<Bitmap>();
-				bitmapList.add(BitmapFactory.decodeResource(getResources(),
-						R.drawable.i_back));
-				bitmapList.add(BitmapFactory.decodeResource(getResources(),
-						R.drawable.i_caledar));
-				bitmapList.add(BitmapFactory.decodeResource(getResources(),
-						R.drawable.i_logo));
+			// }
+			if (requestCode == 200) {
+				//사진 패스를 받아옴
+				ArrayList<PhotoInfo> all_path = (ArrayList<PhotoInfo>) data
+						.getExtras().getSerializable("list");
 
-				// Log.d("path", imagePath);
-				imgFile = new File(imagePath);
-				// Log.d("path", storagePath);
-				// 이 부분이 저장될 파일 위치
-				storageFile = new File("/storage/emulated/0/DCIM/Camera",
-						"a.png");
-				Log.d("path", storageFile.toString());
-				scaledBitmap = ImageResizer.resize(imgFile, 300, 300);
-
-				Log.d("scaledBitmap", scaledBitmap + "");
-				ImageResizer.saveToFile(scaledBitmap, storageFile);
-
-				/* Log.d("resize","resizing!"); */
-
-				// img file bitmap 蹂�꼍
-				if (imgFile.exists()) {
-					mBitmap = BitmapFactory.decodeFile(imgFile
-							.getAbsolutePath());
-					Log.d("mBitmap", imgFile.getAbsolutePath() + "");
-					// getCroppedBitmap(mBitmap);
-					for (int i = 0; i < bitmapList.size(); i++) {
-						// 리사이징 안할거니까 imgFile없애야함
-						layoutGridPhotoAlbum.addView(new AlbumImgCell(
-								PhotoputActivity.this, bitmapList.get(i),
-								imgFile));
-					}
-					Log.e("鍮꾪듃留�濡쒕뱶", "�깃났");
+				for (int i = 0; i < all_path.size(); i++) {
+					//받아온 패스로 파일 만든다
+					all_path.get(i).setFile(new File(all_path.get(i).getPath()));
+					imgFile = new File(all_path.get(i).getPath());
+					layoutGridPhotoAlbum.addView(new AlbumImgCell(
+							PhotoputActivity.this, all_path.get(i).getFile()));
 				}
-
-				BitmapDrawable bd = (BitmapDrawable) this.getResources()
-						.getDrawable(R.drawable.i_photo_gray_mask318x318);
-				Bitmap coverBitmap = bd.getBitmap();
-
-				// constructor
-				// mBitmap��李띿� �ъ쭊 �ｊ린
-				// cover��洹몃�濡�
-
-				// photoEditor photoEdit = new photoEditor(mBitmap, coverBitmap,
-				// photoAreaWidth, photoAreaHeight);
-				// // resize
-				// // crop roun
-				// // overay cover
-				//
-				// // �닿굅�섎㈃ �대�吏��뗫맖
-				// mBitmap = photoEdit.editPhotoAuto();
-				// btn_picbtn.setImageBitmap(mBitmap);
+				//저 for문에서 이미지 뷰에 추가하고 파일 생성하니까 아마 이부분에서 서버 코드 추가하면 될꺼에요~
 
 			}
 		}
 	}
 
-	private String getRealPathFromURI(Uri contentUri) {
-		String path = null;
-		String[] proj = { MediaStore.MediaColumns.DATA };
-		Cursor cursor = getContentResolver().query(contentUri, proj, null,
-				null, null);
-		if (cursor.moveToFirst()) {
-			int column_index = cursor
-					.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-			path = cursor.getString(column_index);
-		}
-		cursor.close();
-		return path;
-	}
-			
-	
+	private String getRealPathFromURI(Uri selectedVideoUri,
+			ContentResolver contentResolver)
+			throws UnsupportedEncodingException {
+		String filePath;
+		String[] filePathColumn = { MediaStore.MediaColumns.DATA };
 
+		Cursor cursor = contentResolver.query(selectedVideoUri, filePathColumn,
+				null, null, null);
+		cursor.moveToFirst();
+
+		int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+		filePath = cursor.getString(columnIndex);
+		cursor.close();
+		String result = java.net.URLDecoder.decode(filePath, "UTF-8");
+		return result;
+	}
 }
