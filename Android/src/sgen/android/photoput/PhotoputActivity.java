72c3cc81b703ce73b410b564f43c957a9892b40d
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-11 11:35:21 +0000

    포토풋 데이앨범 완성

--- Android/src/sgen/android/photoput/PhotoputActivity.java
+++ Android/src/sgen/android/photoput/PhotoputActivity.java
@@ -4,12 +4,9 @@ import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
-import sgen.android.multigallery.CustomGallery;
 import sgen.android.multigallery.PhotoInfo;
-import sgen.image.resizer.ImageResizer;
 import sgen.sgen_pourney.AskActivity;
 import sgen.sgen_pourney.CoverActivity;
-import sgen.sgen_pourney.CoverCell;
 import sgen.sgen_pourney.LoginActivity;
 import sgen.sgen_pourney.R;
 import sgen.sgen_pourney.SimpleSideDrawer;
@@ -19,7 +16,6 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -71,8 +67,14 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 	private ProgressDialog mLoagindDialog;
 	private GridView gridviewPhotoAlbum;
 
+	private int travel = 3;
+	private ArrayList<DayAlbum> dayalbumList;
+
 	// private ImageAdapter mListAdapter;
 	// private ArrayList<ThumbImageInfo> mThumbImageInfoList;
+
+	// 선택된 사진들이 몇번째 데이앨범인지
+	private int i_dayalbum;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -98,14 +100,29 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 		logoutBtn = (Button) findViewById(R.id.log_out_text);
 		logoutBtn.setOnClickListener(this);
 		layoutAlbum = (LinearLayout) findViewById(R.id.layoutAlbum);
-		layoutAlbum.addView(new DayAlbum(PhotoputActivity.this));
+		// for (int i = 0; i < travel; i++) {
+		// layoutAlbum.addView(new DayAlbum(PhotoputActivity.this));
+		// }
+		init();
 		gridviewPhotoAlbum = (GridView) findViewById(R.id.gridviewPhotoAlbum);
 		layoutGridPhotoAlbum = (GridLayout) findViewById(R.id.layoutGridPhotoAlbum);
+
 		friendList = (ImageButton) findViewById(R.id.imgBack);
 		popupLocation = (TextView) findViewById(R.id.textPeople);
 
 		friendList.setOnClickListener(this);
 
+	}
+
+	private void init() {
+		// 여행일정만큼 어레이리스트 생성
+		dayalbumList = new ArrayList<DayAlbum>();
+		for (int i = 0; i < travel; i++) {
+			dayalbumList.add(new DayAlbum(PhotoputActivity.this, i));
+		}
+		for (int i = 0; i < travel; i++) {
+			layoutAlbum.addView(dayalbumList.get(i));
+		}
 	}
 
 	public void onClick(View v) {
@@ -144,12 +161,13 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 					return false;
 				}
 			});
-for(int i=0;i<7;i++){
-			((GridLayout) friendListPopupWindow.getContentView()
-					.findViewById(R.id.friendlistpopupback))
-					.addView(new FriendListCell(this));
-			friendListPopupWindow.showAsDropDown(popupLocation, -475, 27);
-		}}
+			for (int i = 0; i < 7; i++) {
+				((GridLayout) friendListPopupWindow.getContentView()
+						.findViewById(R.id.friendlistpopupback))
+						.addView(new FriendListCell(this));
+				friendListPopupWindow.showAsDropDown(popupLocation, -475, 27);
+			}
+		}
 
 	}
 
@@ -158,85 +176,23 @@ for(int i=0;i<7;i++){
 		// TODO Auto-generated method stub
 		Log.d("PhotoputActivity", "onActivityResult");
 		Log.d("PhotoputActivity", resultCode + "");
-		if (resultCode == RESULT_OK) {
-			// Log.d("PhotoputActivity", "requestCode==RESULT_OK");
-			// if (requestCode == SELECT_PICTURE) {
-			// currImageURI = data.getData();
-			// Log.d("KJK", "URI : " + currImageURI.toString());
-			// // �ㅼ젣 �덈�二쇱냼瑜�諛쏆븘��
-			// imagePath = getRealPathFromURI(data.getData());
-			// Log.d("KJK", "URI : " + currImageURI.toString());
-			// Log.d("KJK", "Real Path : " + imagePath);
-			//
-			// // sample bitmaplist
-			// ArrayList<Bitmap> bitmapList = new ArrayList<Bitmap>();
-			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
-			// R.drawable.i_back));
-			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
-			// R.drawable.i_caledar));
-			// bitmapList.add(BitmapFactory.decodeResource(getResources(),
-			// R.drawable.i_logo));
-			//
-			// // Log.d("path", imagePath);
-			// imgFile = new File(imagePath);
-			// // Log.d("path", storagePath);
-			// // 이 부분이 저장될 파일 위치
-			// storageFile = new File("/storage/emulated/0/DCIM/Camera",
-			// "a.png");
-			// Log.d("path", storageFile.toString());
-			// scaledBitmap = ImageResizer.resize(imgFile, 300, 300);
-			//
-			// Log.d("scaledBitmap", scaledBitmap + "");
-			// ImageResizer.saveToFile(scaledBitmap, storageFile);
-			//
-			// /* Log.d("resize","resizing!"); */
-			//
-			// // img file bitmap 蹂�꼍
-			// if (imgFile.exists()) {
-			// mBitmap = BitmapFactory.decodeFile(imgFile
-			// .getAbsolutePath());
-			// Log.d("mBitmap", imgFile.getAbsolutePath() + "");
-			// // getCroppedBitmap(mBitmap);
-			// for (int i = 0; i < bitmapList.size(); i++) {
-			// // 리사이징 안할거니까 imgFile없애야함
-			// layoutGridPhotoAlbum.addView(new AlbumImgCell(
-			// PhotoputActivity.this, bitmapList.get(i),
-			// imgFile));
-			// }
-			// Log.e("鍮꾪듃留�濡쒕뱶", "�깃났");
-			// }
-			//
-			// BitmapDrawable bd = (BitmapDrawable) this.getResources()
-			// .getDrawable(R.drawable.i_photo_gray_mask318x318);
-			// Bitmap coverBitmap = bd.getBitmap();
-			//
-			// // constructor
-			// // mBitmap��李띿� �ъ쭊 �ｊ린
-			// // cover��洹몃�濡�
-			//
-			// // photoEditor photoEdit = new photoEditor(mBitmap, coverBitmap,
-			// // photoAreaWidth, photoAreaHeight);
-			// // // resize
-			// // // crop roun
-			// // // overay cover
-			// //
-			// // // �닿굅�섎㈃ �대�吏��뗫맖
-			// // mBitmap = photoEdit.editPhotoAuto();
-			// // btn_picbtn.setImageBitmap(mBitmap);
 
-			// }
+		if (resultCode == RESULT_OK) {
 			if (requestCode == 200) {
+				// 선택된 데이앨범의 i값을 받아온다
+				i_dayalbum = data.getIntExtra("i_dayalbum", 300);
+				Log.d("Photoput : i_dayalbum ", i_dayalbum + "");
 				// 사진 패스를 받아옴
 				ArrayList<PhotoInfo> all_path = (ArrayList<PhotoInfo>) data
 						.getExtras().getSerializable("list");
 
 				for (int i = 0; i < all_path.size(); i++) {
-					// 받아온 패스로 파일 만든다
+					// 받아온 패스로 파일 만들어서 레이아웃 그리드 앨범에 추가한다.
 					all_path.get(i)
 							.setFile(new File(all_path.get(i).getPath()));
-					imgFile = new File(all_path.get(i).getPath());
-					layoutGridPhotoAlbum.addView(new AlbumImgCell(
-							PhotoputActivity.this, all_path.get(i).getFile()));
+					dayalbumList.get(i_dayalbum).addLayoutGridalbum(
+							new AlbumImgCell(PhotoputActivity.this, all_path
+									.get(i).getFile()));
 				}
 				// 저 for문에서 이미지 뷰에 추가하고 파일 생성하니까 아마 이부분에서 서버 코드 추가하면 될꺼에요~
 
