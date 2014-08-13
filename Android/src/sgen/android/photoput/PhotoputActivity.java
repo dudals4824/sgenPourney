115934b506bd000d34286b2511724334cfd7a526
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-13 05:18:35 +0000

    PhotoputActivity

--- Android/src/sgen/android/photoput/PhotoputActivity.java
+++ Android/src/sgen/android/photoput/PhotoputActivity.java
@@ -1,15 +1,33 @@
 package sgen.android.photoput;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 import sgen.DTO.TripDTO;
+
 import sgen.DTO.UserDTO;
 import sgen.android.multigallery.PhotoInfo;
 import sgen.application.PourneyApplication;
@@ -43,12 +61,12 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -109,6 +127,18 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 	// 사진 가져오는
 	private int serverResponseCode = 0;
 	private ProgressDialog dialog = null;
+	private GetFilename get;
+	private ImageDownloader imagedown;
+	private ImageUploader[] upload;
+	private String[] imagepath;
+	private int endNum = 0;
+	private int pixNum = 0;
+	private ImageView downloadedImg;
+	private ProgressDialog simpleWaitDialog;
+	private String downloadUrl = "http://54.178.166.213/test/";
+	private String[] urllist = null;
+	private String addUrl = null;
+	private String upLoadServerUri = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -285,7 +315,13 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 							new AlbumImgCell(PhotoputActivity.this, all_path
 									.get(i).getFile()));
 				}
-				// 저 for문에서 이미지 뷰에 추가하고 파일 생성하니까 아마 이부분에서 서버 코드 추가하면 될꺼에요~
+				dialog = ProgressDialog.show(PhotoputActivity.this, "",
+						"Uploading file...", true);
+				upload=new ImageUploader[all_path.size()];
+				for (int i = 0; i < all_path.size(); i++) {
+					upload[i] = new ImageUploader();
+					upload[i].execute(imagepath[i]);
+				}
 
 			}
 		}
@@ -332,6 +368,357 @@ public class PhotoputActivity extends Activity implements OnClickListener {
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
+		}
+
+	}//end of ProfileImageSetter
+	
+	public class ImageUploader extends AsyncTask<String, String, Integer>{
+
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+
+			// 사진을 서버로 업로드 완료한 후에
+			// 서버에 있는 사진을 다시 안드로이드에 뿌려주기위해
+			// 방금 업로드한 사진파일이름 받아오는 부분
+			// 같이 여행을 하는 사람들이 볼 수 있어야 하므로 trip_id를 파라미터로 전송
+			// 아래 트립아이디는 임의로 해논것입니다!
+			String trip_id = "101";
+			get = new GetFilename();
+			get.execute(trip_id);
+		}
+
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
+		// pre-background-post
+		@Override
+		protected Integer doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+
+			String sourceFileUri = arg0[0];
+			HttpURLConnection conn = null;
+			DataOutputStream dos = null;
+			String lineEnd = "\r\n";
+			String twoHyphens = "--";
+			String boundary = "*****";
+			int bytesRead, bytesAvailable, bufferSize;
+			byte[] buffer;
+			int maxBufferSize = 1 * 1024 * 1024;
+			File sourceFile = new File(sourceFileUri);
+			if (!sourceFile.isFile()) {
+				Log.d("uploadFile", "Source File Does not exist");
+				return null;
+			}
+			try { // open a URL connection to the <span id="IL_AD7"
+					// class="IL_AD">Servlet</span>
+				FileInputStream fileInputStream = new FileInputStream(
+						sourceFile);
+				URL url = new URL(upLoadServerUri);
+				conn = (HttpURLConnection) url.openConnection(); // Open a HTTP
+				// connection to
+				// the URL
+				conn.setDoInput(true); // Allow Inputs
+				conn.setDoOutput(true); // Allow Outputs
+				conn.setUseCaches(false); // Don't use a Cached Copy
+				conn.setRequestMethod("POST");
+				conn.setRequestProperty("Connection", "Keep-Alive");
+				conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+				conn.setRequestProperty("Content-Type",
+						"multipart/form-data;boundary=" + boundary);
+				conn.setRequestProperty("uploaded_file", sourceFileUri);
+				dos = new DataOutputStream(conn.getOutputStream());
+
+				dos.writeBytes(twoHyphens + boundary + lineEnd);
+				dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+						+ sourceFileUri + "\"" + lineEnd);
+				dos.writeBytes(lineEnd);
+
+				bytesAvailable = fileInputStream.available(); // create a buffer
+																// of
+				// maximum size
+
+				bufferSize = Math.min(bytesAvailable, maxBufferSize);
+				buffer = new byte[bufferSize];
+
+				// read file and write it into form...
+				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+				while (bytesRead > 0) {
+					dos.write(buffer, 0, bufferSize);
+					bytesAvailable = fileInputStream.available();
+					bufferSize = Math.min(bytesAvailable, maxBufferSize);
+					bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+				}
+
+				// send multipart form data necesssary after file data...
+				dos.writeBytes(lineEnd);
+				dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+				// Responses from the server (code and message)
+				serverResponseCode = conn.getResponseCode();
+				String serverResponseMessage = conn.getResponseMessage();
+
+				Log.i("uploadFile", "HTTP Response is : "
+						+ serverResponseMessage + ": " + serverResponseCode);
+				if (serverResponseCode == 200) {
+					runOnUiThread(new Runnable() {
+						public void run() {
+							Toast.makeText(PhotoputActivity.this,
+									"File Upload Complete.", Toast.LENGTH_SHORT)
+									.show();
+						}
+					});
+				}
+
+				// close the streams //
+				fileInputStream.close();
+				dos.flush();
+				dos.close();
+
+			} catch (MalformedURLException ex) {
+				dialog.dismiss();
+				ex.printStackTrace();
+				Log.d("Upload file to server", "error: " + ex.getMessage(), ex);
+			} catch (Exception e) {
+				dialog.dismiss();
+				e.printStackTrace();
+				Log.d("Upload file to server Exception",
+						"Exception : " + e.getMessage(), e);
+			}
+			dialog.dismiss();
+
+			return serverResponseCode;
+
+		}
+
+	}//end of ImageUploader
+	public class GetFilename extends AsyncTask<String, String, String>{
+
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+
+			// 이미지 여러개 다운받을 때 이미지 url들이 적힌 리스트를 파라미터로 전송
+			imagedown = new ImageDownloader();
+			imagedown.execute(urllist);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+
+			InputStream is = null;
+			StringBuilder sb = null;
+			String filename = null;
+			String result = null;
+
+			ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			nameValuePairs.add(new BasicNameValuePair("trip_id", arg0[0]));
+
+			try {
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(
+						"http://54.178.166.213/getPhotoFilename.php");
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				HttpResponse response = httpclient.execute(httppost);
+				HttpEntity entity = response.getEntity();
+				is = entity.getContent();
+
+			} catch (Exception e) {
+				Log.d("log_tag", "error in http connection" + e.toString());
+			}
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "iso-8859-1"), 8);
+				sb = new StringBuilder();
+				sb.append(reader.readLine() + "\n");
+				String line = "0";
+
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+
+				is.close();
+				result = sb.toString();
+				Log.d("getFile_logMsg", result); // result 가 null이지???
+
+			} catch (Exception e) {
+				Log.d("getFile_log_tag",
+						"Error converting result " + e.toString());
+			}
+
+			// 이미지 url list 받아오는 부분
+			try {
+				JSONArray jArray = new JSONArray(result);
+				JSONObject json_data = null;
+
+				pixNum = jArray.length();
+				Log.d("pixNum", Integer.toString(pixNum));
+				urllist = new String[pixNum];
+				for (int i = 0; i < jArray.length(); i++) {
+					json_data = jArray.getJSONObject(i);
+					filename = json_data.getString("photo_filename");
+					Log.d("filename", filename);
+
+					// file이름 받아온 후,
+					// 사진파일들이 저장되어있는 폴더 url에
+					// 파일이름 string을 합쳐서 url list에 넣음
+					addUrl = filename;
+					addUrl = downloadUrl + addUrl;
+					urllist[i] = addUrl;
+
+					Log.d("urlList" + i, urllist[i]);
+					// list에 다 넣은 후 post에서 다운로드 이미지 함수 호출
+
+				}
+
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+
+			return null;
+		}
+
+	}//end of GetfileName
+	public class ImageDownloader extends AsyncTask<String[], String, Bitmap>{
+
+		@Override
+		protected void onCancelled() {
+			// TODO Auto-generated method stub
+			super.onCancelled();
+		}
+
+
+
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+
+			// 이미지 여러개 다운받을 때 이미지 url들이 적힌 리스트를 파라미터로 전송
+			imagedown = new ImageDownloader();
+			imagedown.execute(urllist);
+		
+		}
+
+
+
+		@Override
+		protected void onPreExecute() {
+			// TODO Auto-generated method stub
+			super.onPreExecute();
+		}
+
+		protected String doInBackground(String... arg0) {
+			// TODO Auto-generated method stub
+
+			InputStream is = null;
+			StringBuilder sb = null;
+			String filename = null;
+			String result = null;
+
+			ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			nameValuePairs.add(new BasicNameValuePair("trip_id", arg0[0]));
+
+			try {
+				HttpClient httpclient = new DefaultHttpClient();
+				HttpPost httppost = new HttpPost(
+						"http://54.178.166.213/getPhotoFilename.php");
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				HttpResponse response = httpclient.execute(httppost);
+				HttpEntity entity = response.getEntity();
+				is = entity.getContent();
+
+			} catch (Exception e) {
+				Log.d("log_tag", "error in http connection" + e.toString());
+			}
+
+			try {
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "iso-8859-1"), 8);
+				sb = new StringBuilder();
+				sb.append(reader.readLine() + "\n");
+				String line = "0";
+
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+
+				is.close();
+				result = sb.toString();
+				Log.d("getFile_logMsg", result); // result 가 null이지???
+
+			} catch (Exception e) {
+				Log.d("getFile_log_tag",
+						"Error converting result " + e.toString());
+			}
+
+			// 이미지 url list 받아오는 부분
+			try {
+				JSONArray jArray = new JSONArray(result);
+				JSONObject json_data = null;
+
+				pixNum = jArray.length();
+				Log.d("pixNum", Integer.toString(pixNum));
+				urllist = new String[pixNum];
+				for (int i = 0; i < jArray.length(); i++) {
+					json_data = jArray.getJSONObject(i);
+					filename = json_data.getString("photo_filename");
+					Log.d("filename", filename);
+
+					// file이름 받아온 후,
+					// 사진파일들이 저장되어있는 폴더 url에
+					// 파일이름 string을 합쳐서 url list에 넣음
+					addUrl = filename;
+					addUrl = downloadUrl + addUrl;
+					urllist[i] = addUrl;
+
+					Log.d("urlList" + i, urllist[i]);
+					// list에 다 넣은 후 post에서 다운로드 이미지 함수 호출
+
+				}
+
+			} catch (JSONException e1) {
+				e1.printStackTrace();
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+			}
+
+			return null;
+		}
+
+		@Override
+		protected Bitmap doInBackground(String[]... params) {
+			// TODO Auto-generated method stub
+			return null;
 		}
 
 	}
