a1efef19f9620c6a48a50a056bf453dc71753ffe
dut4824 <dut4824@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-15 12:05:00 +0000

    AskActivity 글씨체 바꿈

--- Android/src/sgen/sgen_pourney/AskActivity.java
+++ Android/src/sgen/sgen_pourney/AskActivity.java
@@ -3,19 +3,39 @@ package sgen.sgen_pourney;
 import sgen.sgen_pourney.R;
 import sgen.sgen_pourney.R.layout;
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 
 public class AskActivity extends Activity {
-    @Override
+    
+    private TextView e_mail;
+    private TextView e_mail_address;
+    private TextView call;
+    private TextView call_info;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask_layout);
+        
+        e_mail=(TextView)findViewById(R.id.e_mail);
+        e_mail_address=(TextView)findViewById(R.id.e_mail_address);
+        call=(TextView)findViewById(R.id.call);
+        call_info=(TextView)findViewById(R.id.call_info);
+        String fontpath="fonts/WalbaumBook-BoldItalic.otf";
+        
+        Typeface tf = Typeface.createFromAsset(getAssets(),
+				fontpath);
+		e_mail.setTypeface(tf);
+		e_mail_address.setTypeface(tf);
+		call.setTypeface(tf);
+		call_info.setTypeface(tf);
+		
     }
 
 }
