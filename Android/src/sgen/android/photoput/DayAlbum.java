feccdbd8b915f943e342933e9a1cae8076bf471a
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-16 13:23:26 +0000

    DayAlbum

--- Android/src/sgen/android/photoput/DayAlbum.java
+++ Android/src/sgen/android/photoput/DayAlbum.java
@@ -8,10 +8,13 @@ import sgen.sgen_pourney.R.layout;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.CheckBox;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
@@ -29,49 +32,50 @@ public class DayAlbum extends LinearLayout {
 
 	public DayAlbum(Context context, int i, String date) {
 		super(context);
-		mDate=date;
+		mDate = date;
 		mContext = context;
+
 		initMarbleView(i);
 		// TODO Auto-generated constructor stub
 	}
 
 	private void initMarbleView(int intent_date) {
 
-		
 		String infService = Context.LAYOUT_INFLATER_SERVICE;
 		LayoutInflater li = (LayoutInflater) getContext().getSystemService(
 				infService);
 		View v = li.inflate(R.layout.dayalbum, this, false);
 		addView(v);
-
 		textDay = (TextView) findViewById(R.id.textDay);
 		textPhotoNum = (TextView) findViewById(R.id.textPhotoNum);
 		layoutGridPhotoAlbum = (GridLayout) findViewById(R.id.layoutGridPhotoAlbum);
 
 		textDay.setText(mDate);
-		
-		layoutGridPhotoAlbum.addView(new AlbumImgBtnCell(mContext,intent_date));
+
+		layoutGridPhotoAlbum
+				.addView(new AlbumImgBtnCell(mContext, intent_date));
 	}
-	public void addLayoutGridalbum(AlbumImgCell albumImgCell){
+
+	public void addLayoutGridalbum(AlbumImgCell albumImgCell) {
 		Log.d("Dayalbum", "addLayoutGridalbum called");
 		layoutGridPhotoAlbum.addView(albumImgCell);
 	}
-	public ArrayList<String> getCheckedImageArray(){
-		ArrayList<String> checkedList= new ArrayList<String>();
-		Log.d("layoutGridPhotoAlbum.getChildCount()", layoutGridPhotoAlbum.getChildCount()+"");
-		for (int i = 1; i <= layoutGridPhotoAlbum.getChildCount(); i++) {
-			View v=layoutGridPhotoAlbum.getChildAt(i);
-			CheckBox checked;
-			checked=(CheckBox)v.findViewById(R.id.checkImage);
-			if(isCheckedImage(checked))
-				checkedList.add(i+"");
-			
+
+	public ArrayList<String> getCheckedImageArray() {
+		ArrayList<String> checkedList = new ArrayList<String>();
+
+		for (int i = 1; i < layoutGridPhotoAlbum.getChildCount(); i++) {
+				View v=(View)layoutGridPhotoAlbum.getChildAt(i);
+				CheckBox checked;
+				checked = (CheckBox) v.findViewById(R.id.checkImage);
+				if (isCheckedImage(checked))
+					checkedList.add(i + "");
 		}
 		return checkedList;
 	}
-	
-	public Boolean isCheckedImage(CheckBox checked){
-		if(checked.isChecked())
+
+	public Boolean isCheckedImage(CheckBox checked) {
+		if (checked.isChecked())
 			return true;
 		else
 			return false;
