98af1d214fdb4e4c40436272a690b43ac3e4de0c
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-19 04:57:55 +0000

    DayAlbumAdapter

--- Android/src/sgen/android/photoput/DayAlbumAdapter.java
+++ Android/src/sgen/android/photoput/DayAlbumAdapter.java
@@ -1,15 +1,19 @@
 package sgen.android.photoput;
 
-import sgen.android.multigallery.GalleryAdapter.ViewHolder;
 import sgen.sgen_pourney.R;
+import android.provider.ContactsContract.Data;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class DayAlbumAdapter extends BaseAdapter {
 	private LayoutInflater inflater;
-
+	private ImageLoader imageLoader;
+	
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
@@ -36,9 +40,23 @@ public class DayAlbumAdapter extends BaseAdapter {
 		if (convertView == null) {
 
 			convertView = inflater.inflate(R.layout.album_img_cell, null);
+			holder=new ViewHolder();
+			holder.imgQueue=(ImageView)convertView.findViewById(R.id.imgPhoto);
+			convertView.setTag(holder);
 			
+			
+		}else{
+			holder=(ViewHolder)convertView.getTag();
 		}
+		holder.imgQueue.setTag(position);
+		
+	//	imageLoader.displayImage("filr://"+Data.getContactLookupUri(resolver, dataUri)
+			
 		return convertView;
+	}
+	public class ViewHolder {
+		ImageView imgQueue;
+		ImageView imgQueueMultiSelected;
 	}
 
 }
