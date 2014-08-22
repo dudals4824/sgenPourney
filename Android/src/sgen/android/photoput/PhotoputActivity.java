33d6681def6e7495ec6755ae8c11f62aef00fb87
dbwls878 <dbwls878@11674698-1cfd-0410-beed-d2c9834c560c>
2014-08-22 07:59:56 +0000

    PhotoputActivity

--- Android/src/sgen/android/photoput/PhotoputActivity.java
+++ Android/src/sgen/android/photoput/PhotoputActivity.java
@@ -467,23 +467,6 @@ public class PhotoputActivity extends Activity implements OnClickListener,
 							new AlbumImgCell(PhotoputActivity.this, bm, photo,
 									user.getUserId()));
 				}
-
-				/*
-				 * // 서버에 사진 업로드 dialog =
-				 * ProgressDialog.show(PhotoputActivity.this, "",
-				 * "Uploading file...", true); // 한 날짜만 될 듯, 한번만 조회해서 18일것만 서버에서
-				 * 조회하게 될 것 데이트 자체를 리스트로 받아서 for (int i = 0; i <
-				 * all_path.size(); i++) { Log.d("photoput", "upload(" + i +
-				 * ")"); trip.setPhotoCnt(trip.getPhotoCnt() + 1); // upload[i]
-				 * = new ImageUploader(); //
-				 * upload[i].execute(all_path.get(i).getPath()); photoUploader =
-				 * new PhotoUploader( all_path.get(i).getPath(),
-				 * user.getUserId(), trip.getTripId(), i_dayalbum);
-				 * photoUploader.start(); try { photoUploader.join(); } catch
-				 * (InterruptedException e) { e.printStackTrace(); } }
-				 * dialog.dismiss(); // updatephotodate = new UpdatePhotodate();
-				 * // updatephotodate.execute(trip);
-				 */
 			}
 
 		}
